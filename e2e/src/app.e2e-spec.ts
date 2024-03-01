c3580ddb73f11312a7d5db7a3df450f6e43bd0e8
Mathew Kadambatt <mathew.kadambatt@walmart.com>
2024-03-01 17:56:37 +0530

    changes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,11 +1,11 @@
 import { browser, element, by, logging } from 'protractor';
 
-describe('first-app-lesson-00 app', () => {
+describe('first-app-lesson-01 app', () => {
 
   beforeEach(() => browser.get(''));
 
   it('should display correct title', async () => {
-    expect(await element.all(by.css('h1')).get(0).getText()).toEqual('Default');
+    expect(await element.all(by.css('h1')).get(0).getText()).toEqual('Hello world!');
   });
 
   afterEach(async () => {
